dc28f7d3c7814cd4f775ecdda886f46f01fd5085
Tejaswi Dhyani <dollydhyani6@gmail.com>
2023-05-17 08:21:49 -0700

    hey portfolio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let nav = document.querySelector(".navbar");
+
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+        
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide when click on links
+let navLINKS = document.querySelectorAll(".nav-link");
+let navCollapse=document.querySelector(".navbar-collapse.collapse");
+navLINKS.forEach(function(a){
+a.addEventListener("click",function(){
+    navCollapse.classList.remove("show");
+})
+})
